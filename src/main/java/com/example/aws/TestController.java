c1a6559ae5b2a1856449f15ed0ce0816a8b69843
ChangHoMoon <tony9911@naver.com>
2024-08-15 19:36:53 +0900

    [feat] test엔티티 생성 및 출력 api

--- src/main/java/com/example/aws/TestController.java
+++ src/main/java/com/example/aws/TestController.java
@@ -1,16 +1,22 @@
 package com.example.aws;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestRepository testRepository;
 
     @GetMapping("/")
     @ResponseBody
-    public String test(){
-        return "hello_aws";
+    public String test() {
+        TestEntity entity= testRepository.findById(1L).orElse(null);
+        return entity.getName()+":"+entity.getPassword();
     }
 }
---
+++ src/main/java/com/example/aws/TestRepository.java
@@ -0,0 +1,7 @@
+package com.example.aws;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<TestEntity,Long> {
+
+}
