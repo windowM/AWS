79c559d5134f05d5d04f424c0f4ccdcf663df6d7
ChangHoMoon <tony9911@naver.com>
2024-08-16 23:41:05 +0900

    [fix] 배포 자동화를 위해 초기화
    - db정보를 .env파일에 숨기려고 하다 보니 githubactions를 수행하는 과정에서 빌드가 실패.
    - 이후 docker를 사용하기로 하고 우선 배포 자동화만 테스트

--- src/main/java/com/example/aws/TestController.java
+++ src/main/java/com/example/aws/TestController.java
@@ -1,22 +1,13 @@
 package com.example.aws;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
-@Controller
-@RequiredArgsConstructor
+@RestController
 public class TestController {
 
-    private final TestRepository testRepository;
-
     @GetMapping("/")
-    @ResponseBody
     public String test() {
-        TestEntity entity= testRepository.findById(1L).orElse(null);
-        return entity.getName()+":"+entity.getPassword()+"update deploy";
+        return "deploy test";
     }
 }
--- src/main/java/com/example/aws/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aws;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity,Long> {
-
-}
